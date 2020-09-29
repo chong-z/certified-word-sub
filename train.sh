dc554e83cc5d1648205f07767d62e0b2dadea5a9
Chong Zhang <zchoniong@gmail.com>
2020-09-28 23:27:57 -0700

    Add bow models

--- train.sh
+++ train.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 python src/train.py classification cnn model_data/cnn_normal --data-cache-dir .cache -d 100 --pool mean -T 10 --dropout-prob 0.2 -b 32 --save-best-only
-
 python src/train.py classification cnn model_data/cnn_cert --data-cache-dir .cache -d 100 --pool mean -T 60 --full-train-epochs 20 -c 0.8 --dropout-prob 0.2 -b 32 --save-best-only
+
+
+python src/train.py classification bow model_data/bow_normal -d 100 --pool mean -T 60 --dropout-prob 0.2 -b 32 --save-best-only
+python src/train.py classification bow model_data/bow_cert -d 100 --pool mean -T 60 --dropout-prob 0.2 --full-train-epochs 20 -c 0.8 -b 32 --save-best-only
